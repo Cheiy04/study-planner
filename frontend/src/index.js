8e68d1ef6ef60090327696febf08f2c1d33362e9
Cheiy04 <94778257+Cheiy04@users.noreply.github.com>
2026-04-16 01:09:59 +0200

    Update ReactDOM usage for new API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>,\n  document.getElementById('root')\n);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
