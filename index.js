835ac5387be45b3ce6bc7eda0a68a597816fdfb2
Cheiy04 <94778257+Cheiy04@users.noreply.github.com>
2026-04-16 01:21:06 +0200

    Change server port from 3000 to 5000

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/health', (req, res) => {
 dbInit();
 
 // Start the server
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
